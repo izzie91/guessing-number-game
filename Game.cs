51b88ec6f813c83ddb8a7dd2157976640fb6aeb5
izzie91 <isa910823@gmail.com>
2024-09-04 13:24:37 +0200

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessing_number_game;
+
+public class Game
+{
+    private Random _rnd = new Random();
+    private readonly int _generatedRandomNumber;
+    private bool _gameIsWon = false;
+
+    public event EventHandler<GuessNumberDataEventArgs> GameEvent;
+
+    public Game()
+    {
+        // Generate a random number between 1 and 6 (like a dice)
+        _generatedRandomNumber = _rnd.Next(1, 6);
+        Console.WriteLine("My generated number is " + _generatedRandomNumber);
+    }
+
+    public void CompareUsersNumber(int guessedNumber)
+    {      
+        string message = "Wrong, keep going! :(";
+
+        if (guessedNumber == _generatedRandomNumber)
+        {
+            message = "You guessed it!! Well done! :)";
+            _gameIsWon = true;
+        }      
+
+        OnNumberEvaluated(new GuessNumberDataEventArgs
+        {
+            GuessNumberGameOutputMessage = message,
+            GameIsWon = _gameIsWon
+        });
+    }
+
+    protected virtual void OnNumberEvaluated(GuessNumberDataEventArgs e) // event raising method
+    {
+        EventHandler<GuessNumberDataEventArgs> handler = GameEvent;
+        if (handler != null)
+        {
+            handler(this, e);
+        }
+    }
+}
+
+public class GuessNumberDataEventArgs : EventArgs
+{
+    public string GuessNumberGameOutputMessage { get; set; }
+
+    public bool GameIsWon { get; set; }
+}
+
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace guessing_number_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var instructions = """
+                I will roll a dice to get a reference number.
+                Then, you roll the dice and see if you get the same number as mine.
+                You have 3 chances.
+                """;
+            Console.WriteLine(instructions);
+            Console.WriteLine();
+
+            int counter = 0;
+            bool gameIsWon = false;
+
+            Game game = new Game();
+
+            // Subscribe to the GameEvent
+            game.GameEvent += EventHandlerMethod;
+
+            do
+            {
+                counter++;
+                Console.WriteLine("Please input your number choice:");
+                int userGuessedNumber = Int32.Parse(Console.ReadLine());
+
+                if (userGuessedNumber > 6)
+                {
+                    Console.WriteLine("This is not a number of a dice. You have lost!");
+                    break;
+                }
+
+                game.CompareUsersNumber(userGuessedNumber);
+
+            } while (gameIsWon == false && counter < 3);
+
+            // Unsubscribe from the GameEvent after the game is over
+            game.GameEvent -= EventHandlerMethod;
+
+            void EventHandlerMethod(object sender, GuessNumberDataEventArgs args)
+            {
+                if (!args.GameIsWon && counter == 3)
+                {
+                    Console.WriteLine("You have lost :(!");
+                    return;
+                }
+                Console.WriteLine(args.GuessNumberGameOutputMessage);
+                gameIsWon = args.GameIsWon;
+            }
+        }
+    }
+}
